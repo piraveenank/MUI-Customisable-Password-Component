b2bef0b75ee07ca9538c3397c0e5da8e5e8a8779
Piraveenan Kirupakaran <piraveenankirupakaran@yahoo.com>
2023-08-24 15:58:11 +0100

    Updated package

--- lib/dist/index.d.ts
+++ lib/dist/index.d.ts
@@ -1,1 +1,34 @@
-export { default as MUICustomisablePasswordComponent } from "./utils/CustomisablePassword";
+export type PasswordRequirementNames =
+  | "minLength"
+  | "maxLength"
+  | "specialChar"
+  | "number"
+  | "capital"
+  | "lowercase"
+  | "letter"
+  | "notEmpty";
+
+interface PasswordProps {
+  value: string;
+  valueAgain?: string;
+  minLength?: number;
+  maxLength?: number;
+  validColour?: string;
+  defaultColour?: string;
+  specialCharactersRegex?: RegExp;
+  onChange?: (_: any) => void;
+}
+
+export interface MUI_CustomisablePasswordProps extends PasswordProps {
+  rules?: Partial<{ [key in PasswordRequirementNames]: { label?: string } }>;
+  passwordStrengthBar?: boolean;
+  successColourProgressBar?: string;
+  warningColourProgressBar?: string;
+}
+
+export interface PasswordRequirementProps {
+  valid: boolean;
+  validColour?: string;
+  defaultColour?: string;
+  children?: React.ReactNode;
+}
